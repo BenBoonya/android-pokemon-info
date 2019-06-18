d52ebdb4df7bf88e3d2a95a389f655d51e7b080e
BenBoonya <benbenn223@gmail.com>
2019-06-18 16:19:08 +0700

    first commit

---
+++ app/src/main/java/com/benboonya/pokemoninfo/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.benboonya.pokemoninfo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
